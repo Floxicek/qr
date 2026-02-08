523d59c45ae8a5676eecc159d02afb5b40ecdac4
Jan Rehak <jan@ejhle.net>
2026-02-08 16:11:19 +0100

    🐛 fix redirect

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,6 @@ import { base } from '$app/paths';
 export const load = () => {
     // 302 (Temporary) or 301 (Permanent)
     // We use `base` to ensure it works with your repo name (e.g. /my-repo/v1)
-    throw redirect(302, `${base}/v1/`); 
+    throw redirect(302, `${base}/v1/docs`); 
 };
 </script>
